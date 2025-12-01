4d4d4cb8c014575d0b07ddba0bb6bad2bded91ce
vnclopez <vnclopez@gmail.com>
2025-12-01 19:35:40 -0300

    Estilizando barra de título e tabBar

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 import TelaInicio from "./componentes/TelaInicio";
 import TelaSobre from "./componentes/TelaSobre";
 import TelaVitorias from "./componentes/TelaVitorias";
 
 const Abas = createBottomTabNavigator();
 
+const scrOptions = {
+  headerStyle: {
+    backgroundColor: '#eecb01'
+  },
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  tabBarActiveTintColor: '#ba1718',
+  tabBarInactiveTintColor: '#000',
+  tabBarStyle: {
+    backgroundColor: '#eecb01'
+  },
+  tabBarActiveBackgroundColor: '#fff'
+};
+
+const home = ({ color }) => (<Entypo name="home" size={20} color={color} />);
+const questionCircle = ({ color }) => (<FontAwesome5 name="question-circle" size={20} color={color} />);
+const trophy = ({ color }) => (<FontAwesome5 name="trophy" size={20} color={color} />);
+
 export default function App() {
   return (
     <NavigationContainer>
 
-      <Abas.Navigator>
+      <Abas.Navigator screenOptions={scrOptions}>
 
-        <Abas.Screen name="Início" component={TelaInicio} />
-        <Abas.Screen name="Sobre" component={TelaSobre} />
-        <Abas.Screen name="Vitórias" component={TelaVitorias} />
+        <Abas.Screen name="Início" component={TelaInicio} options={{ tabBarIcon: home }} />
+
+        <Abas.Screen name="Sobre" component={TelaSobre} options={{ tabBarIcon: questionCircle }} />
+
+        <Abas.Screen name="Vitórias" component={TelaVitorias} options={{ tabBarIcon: trophy }} />
 
       </Abas.Navigator>
 
