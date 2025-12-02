48f9fc4703c6fef3de761f5929cc1cbd330edbc8
vnclopez <vnclopez@gmail.com>
2025-12-02 13:29:56 -0300

    Estrutura da tela inicial

--- componentes/TelaInicio/index.js
+++ componentes/TelaInicio/index.js
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Image } from "react-native";
+
+import fundo from '../../assets/fundo.jpg';
+import fotoCapa from '../../assets/foto-capa.jpg';
 
 export default function TelaInicio() {
     return (
-        <View>
-            <Text>TelaInicial</Text>
-        </View>
+        <ImageBackground source={fundo} style={{ flex: 1 }}>
+
+            <Image source={fotoCapa} />
+
+            <View>
+                <Text>Ayrton Senna</Text>
+
+                <Text>
+                    Através desse APP, você vai conhecer um pouco sobre quem foi esse grande piloto.
+                </Text>
+            </View>
+
+        </ImageBackground>
     );
 }
